a5bcb295abfaadeacb8b20a7d63422a5d247da7d
Hitarth Hindocha <hindochahitarth@gmail.com>
2026-02-03 10:11:43 +0530

    feat:Initialize Project Structure

---
+++ src/test/java/org/miniproject/bugnest/BugNestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.miniproject.bugnest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BugNestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
